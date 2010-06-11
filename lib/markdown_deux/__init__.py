19c9ed8f5bbee441de5248f8ddc007d6da5eba77
Trent Mick <trentm@gmail.com>
2010-06-10 23:51:00 -0700

    first crack at this: {% load markdown_deux_tags %}

---
+++ lib/markdown_deux/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Copyright (c) 2008-2010 ActiveState Corp.
+# License: MIT (http://www.opensource.org/licenses/mit-license.php)
+
+r"""A small Django app that provides template tags for Markdown using the
+python-markdown2 library.
+
+See <http://github.com/trentm/django-markdown-deux> for more info.
+"""
+
+__version_info__ = (1, 0, 0)
+__version__ = '1.0.0'
+__author__ = "Trent Mick"
+
+
+def markdown(text, style="default"):
+    if not text:
+        return ""
+    import markdown2
+    print "XXX markdown_deux.markdown(style=%r) -> %r" % (style, get_style(style))
+    
+    return markdown2.markdown(text, **get_style(style))
+
+def get_style(style):
+    from markdown_deux.conf import settings
+    try:
+        return settings.MARKDOWN_DEUX_STYLES[style]
+    except KeyError:
+        return settings.MARKDOWN_DEUX_STYLES.get("default",
+            settings.MARKDOWN_DEUX_DEFAULT_STYLE)
---
+++ lib/markdown_deux/conf/settings.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2010 ActiveState Software Inc.
+
+from django.conf import settings
+
+MARKDOWN_DEUX_HELP_URL = "http://daringfireball.net/projects/markdown/syntax"
+
+MARKDOWN_DEUX_DEFAULT_STYLE = {
+    "extras": {
+        "code-friendly": None,
+    },
+    "safe_mode": "escape",
+}
+
+MARKDOWN_DEUX_STYLES = getattr(settings, "MARKDOWN_DEUX_STYLES",
+    {"default": MARKDOWN_DEUX_DEFAULT_STYLE})
+
