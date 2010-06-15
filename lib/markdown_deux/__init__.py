616d8d7f9c2ad41e41bc05e3ce29a94ec40911bf
Trent Mick <trentm@gmail.com>
2010-06-14 22:09:13 -0700

    next release will be 1.0.1

--- lib/markdown_deux/__init__.py
+++ lib/markdown_deux/__init__.py
@@ -8,8 +8,8 @@ python-markdown2 library.
 See <http://github.com/trentm/django-markdown-deux> for more info.
 """
 
-__version_info__ = (1, 0, 0)
-__version__ = '1.0.0'
+__version_info__ = (1, 0, 1)
+__version__ = '1.0.1'
 __author__ = "Trent Mick"
 
 
